b81f5781f0a4f75bd728c81133eb05b06c74c98b
MartinYiWu <756589808@qq.com>
2018-11-14 10:48:10 +0800

    2

---
+++ booklease/common/common-model/src/main/java/com/person/booklease/App.java
@@ -0,0 +1,13 @@
+package com.person.booklease;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ booklease/lease-euraka/src/main/java/com/person/bookleas/LeaseEurakaApplication.java
@@ -0,0 +1,12 @@
+package com.person.bookleas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseEurakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaseEurakaApplication.class, args);
+    }
+}
